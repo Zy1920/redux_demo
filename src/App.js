2eed2ea4f355da02a27a2468992ca101215e2f44
十年 <18715096913@163.com>
2018-08-05 11:32:45 +0800

    third commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import React from "react"
+import { connect }  from "react-redux"
+import {addGUN,removeGUN,addGUNAsync} from "./index.redux";
 
 class App extends React.Component{
     render(){
         //4.app内部通过属性获取store及相应的方法
-        const store=this.props.store
-        const addGUN=this.props.addGUN
-        const removeGUN=this.props.removeGUN
-        const addGUNAsync=this.props.addGUNAsync
-        const num=store.getState()
         return(
             <div>
-                <h1>现在有机枪{num}把</h1>
-                <button onClick={()=>store.dispatch(addGUN())}>申请武器</button>
-                <button onClick={()=>store.dispatch(removeGUN())}>上交武器</button>
-                <button onClick={()=>store.dispatch(addGUNAsync())}>延迟发放武器</button>
+                <h1>现在有机枪{this.props.num}把</h1>
+                <button onClick={this.props.addGUN}>申请武器</button>
+                <button onClick={this.props.removeGUN}>上交武器</button>
+                <button onClick={this.props.addGUNAsync}>延迟发放武器</button>
             </div>
             )
     }
 }
+const mapStatetoProps=(state)=>{
+    return {num:state}
+}
+const actionCreators={ addGUN, removeGUN, addGUNAsync}
+
+//2.connect负责从外部获取组建需要的参数
+App=connect(mapStatetoProps,actionCreators)(App)
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,20 +1,26 @@
 import React from "react"
 import ReactDom from "react-dom"
 import App from "./App"
-import { createStore ,applyMiddleware } from "redux"
+import { createStore ,applyMiddleware,compose } from "redux"
 import thunk from "redux-thunk"
-import { counter,addGUN,removeGUN ,addGUNAsync} from "./index.redux";
+import { Provider } from "react-redux"
+import { counter} from "./index.redux";
 
 //react和redux建立连接的方式
 //1.引入store 新建一个store
-const store=createStore(counter,applyMiddleware(thunk))
+const store=createStore(counter,compose(
+    applyMiddleware(thunk)
+))
 
-function render(){
-    //2.将store以组件属性的形式传递给子组件
-    ReactDom.render(<App store={store} addGUN={addGUN} removeGUN={removeGUN} addGUNAsync={addGUNAsync} />,document.getElementById("root"))
-}
-render()
+//1.provider组建在应用最外层，传入store即可，只用一次
+ReactDom.render(
+    <Provider store={store}>
+        <App  />
+    </Provider>,
+    document.getElementById("root")
+)
 
-//3.外界用subscribe订阅render函数 状态变化即重新渲染整个界面
-store.subscribe(render)
+
+
+
 
